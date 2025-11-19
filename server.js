98d06d9c56216dafd70655ff60869afbf5a4b6f7
waghmodekaran5595 <waghmodekaran5595@gmail.com>
2025-11-19 20:47:21 +0530

    main

--- server.js
+++ server.js
@@ -24,13 +24,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // middlewares
-app.use(cors({
-  origin: "https://k-shop-bend.vercel.app",
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-}));
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
